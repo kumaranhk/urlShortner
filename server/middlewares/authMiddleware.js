d561c8238c03b994410da6990ca855dd7bcdb6f8
hariharakumaran <hariharakumaran@visailabs.com>
2025-03-03 10:02:11 +0530

    Added print sattement in middleware

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,6 +1,8 @@
 export const isAuthenticated = (req, res, next) => {
+    console.log("Session from the client :", req.session);
+    console.log("Result of is Authenticated : ", req.isAuthenticated());
     if (req.isAuthenticated()) {
         return next();
     }
-    return res.status(401).json({ msg: "Unauthorized" });
+    return res.status(401).json({ msg: "Unauthorized from middleware" });
 };
